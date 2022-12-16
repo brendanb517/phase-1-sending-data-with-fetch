45612ebc1154e292e488dcdb0d8b00d841bc05c8
Brendan Baird <brendan.b517@gmail.com>
2022-12-16 14:00:59 -0500

    commit tree 97ea431f6b38542424cb486e315475f868f96add

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your code here
+function submitData(username, email){
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": username,
+            "email": email
+        }),
+    }
+    return fetch("http://localhost:3000/users", configurationObject);
+}
